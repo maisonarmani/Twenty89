607936504d1fa72cee397cce1468c7441aa16088
Maison Armani <masonarmani38@gmail.com>
2017-08-25 13:05:57 +0100

    latest update

--- index.android.js
+++ index.android.js
@@ -4,24 +4,50 @@ import {
     StyleSheet,
     Text,
     View,
+    Animated
 } from "react-native";
 
 import {Button} from "native-base"
 
-class Twenty89 extends Component{
-    render(){
-        return(
+class Twenty89 extends Component {
+    constructor(props) {
+        super(props);
+        this.animated = new Animated.Value(0);
+
+        this.timing = Animated.timing(this.animated,{
+            toValue:1, duration:1000
+        });
+        this.animate =  this.animate.bind(this);
+
+    }
+
+    animate(){
+        this.timing.start()
+    }
+
+    render() {
+        var opacity = this.animated.interpolate({
+            inputRange:[0,1],
+            outputRange:[0,1]
+        });
+
+        return (
             <View>
-                <Button ><Text>Maison Armani</Text></Button>
-                <Text style={style.text}>Maison Armani</Text>
+                <Button onPress={this.animate}  >
+                    <Text style={{color:"white"}}>Maison Armani</Text>
+                </Button>
+                <Animated.View style={{opacity}}>
+                    <Text style={style.text}>Maison Armani</Text>
+                </Animated.View>
             </View>
         );
     }
 }
 
 const style = StyleSheet.create({
-    "text":{
-        "color":"red"
+    "text": {
+        "color": "red",
+        "fontSize" :20
     }
 })
 AppRegistry.registerComponent('Twelve', () => Twenty89);
